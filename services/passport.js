4f97465c74876865c69d73270381b3b9a642ec39
Jason Vance <jbvance@gmail.com>
2017-07-29 13:55:20 +0000

    Continue user authentication implementation

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +11,18 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('acess token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    //user already exists with given profile id
+                    // call done - first arg is err, second arg is user record
+                    done(null, existingUser);
+                } else {
+                    //No user id, create one now
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+                }
+            })
     })
 );
