c4e3b8540207218fd4cb289b0303f53281483446
Jason Vance <jbvance@gmail.com>
2017-07-29 14:20:30 +0000

    Add serilize and deserialize user passport functionality

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,23 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// the user arg below is whatever user got pulled out of the db
+// from passport.use below, either a new one or an existing one
+// this function takes user and turns it into and id that is
+// then stuffed into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id); //user.id is the id from MongoDB; this is how mongoose reference it
+});
+
+
+// turns an id into a mongoose user model instance
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
