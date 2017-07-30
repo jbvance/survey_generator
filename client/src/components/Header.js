beccc1029c1f5cb74b9d7ce4ad636c495da5054c
Jason Vance <jbvance@gmail.com>
2017-07-30 14:25:48 -0500

    Complete Header component and auth links flow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return '';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +41,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false; //will return false if not logged in because it will be an empty string ("")
     default:
       return state;
   }
