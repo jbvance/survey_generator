726e6d78edc5c9821c1cb31936986a5ee68fcaa1
Jason Vance <jbvance@gmail.com>
2017-08-02 06:22:50 -0500

    Add Stripe payment functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,3 +10,10 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// Take the token from Payments component and post it
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,21 +11,22 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000, //thirty days
-        keys: [keys.cookieKey],
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //thirty days
+    keys: [keys.cookieKey]
+  })
 );
 
 //tell passport it should use cookies to handle authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-console.log("app listening on PORT ", PORT);
+console.log('app listening on PORT ', PORT);
