e60a23c97e1c7df737691254a9f53c25bd22e0b5
Jason Vance <jbvance@gmail.com>
2017-08-03 06:41:20 -0500

    Begin configuring routes for production

--- index.js
+++ index.js
@@ -26,6 +26,25 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// Only run this in production. In dev, create-react-app
+// server handles the react routing stuff
+if (proccess.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  // If any get request comes in for a route, file, or anything,
+  // and we don't have a route already set up for it, look inside the
+  // referenced folder below and try to find it. If it's there, respond with that file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file if it
+  // doesn't recognize the route from anything listed above in the file
+  // this is the catchall in case no other route was recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
