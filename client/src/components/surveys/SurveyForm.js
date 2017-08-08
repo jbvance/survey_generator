81d5d3fc928db166caccc12f5c03b8e392925a2a
Jason Vance <jbvance@gmail.com>
2017-08-08 07:26:20 -0500

    Begin creating custom input component for survey form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,11 +2,26 @@
 import React, { Component } from 'react';
 // reduxForm allows component to communicate with redux store, much
 // like connect function in components
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <SurveyField type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
   render() {
-    return <div>SurveyForm</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
