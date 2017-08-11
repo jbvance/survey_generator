bd7b21119f7531285df61578fec469db71ee0ffd
Jason Vance <jbvance@gmail.com>
2017-08-11 06:40:10 -0500

    Begin form validation and survey review

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -6,12 +6,29 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' }
+  {
+    label: 'Survey Title',
+    name: 'title',
+    noValueError: 'You must provide a title'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    noValueError: 'You must provide a subject'
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    noValueError: 'You  must provide a body'
+  },
+  {
+    label: 'Recipient List',
+    name: 'emails',
+    noValueError: 'You must provide emails'
+  }
 ];
 
 class SurveyForm extends Component {
@@ -31,7 +48,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
@@ -49,9 +66,16 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  if (!values.title) {
-    errors.title = 'You must provide a title';
-  }
+  // Provide an empty string in case no values are passed in because
+  // the form validation runs as soon as the form is loaded and there are
+  //  no emails at that point
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
 
   return errors;
 }
