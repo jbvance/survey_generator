4f4a8f4af7f5c72a17a6a944799164e55bc09b71
Jason Vance <jbvance@gmail.com>
2017-08-16 06:57:01 -0500

    Complete Survey creation UI

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,10 +3,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
-import _ from 'lodash';
+//import _ from 'lodash';
 import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = formFields.map(field => {
     return (
       <div key={field.name}>
@@ -31,7 +32,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
         Back
       </button>
       <button
-        onClick={() => submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
         className="green white-text btn-flat right"
       >
         Send Survey
@@ -46,4 +47,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
